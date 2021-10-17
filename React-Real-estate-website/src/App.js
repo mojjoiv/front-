8e18d414338dbbba94c1069d55029cbc9f7dd485
patrick <patrick@STLs-MacBook-Air.local>
2021-10-17 19:40:35 +0300

    first commit

---
+++ React-Real-estate-website/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import './App.css';
+import {UserContext} from './context/UserContext'
+import Signup from './pages/Signup'
+
+
+
+function App() {
+
+  const [user, setUser] = useState(null)
+  {console.log(user)}
+  
+  return (
+  <UserContext.Provider value={{user, setUser}}>
+  <Signup/>
+   </UserContext.Provider>
+  );
+
+}
+
+
+
+export default App;
